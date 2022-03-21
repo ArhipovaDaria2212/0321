08adf8a711db4c78ca60f8b0ff495c687bda771c
Arhipova Daria <daria221220042017@gmail.com>
2022-03-21 12:33:04 +0700

    2

--- index.php
+++ index.php
@@ -8,10 +8,9 @@
 </head>
 <body> 
     <form action="index.php" method="GET">
-        <span class="text">В какой стране вы живете? </span><input name="a" type="text" value="<?php if (isset($_GET['a'])) echo $_GET['a'] ?>"> <br> <br>
-        <span class="text">А в каком городе? </span><input name="b" type="text" value="<?php if (isset($_GET['b'])) echo $_GET['b'] ?>"><br> <br>
+        <span class="text">Введите год </span><input name="a" type="text" value="<?php if (isset($_GET['a'])) echo $_GET['a']; else echo '2022'; ?>"> <br> <br>
         <input type="submit" value="Ok">
     </form><br>
-    <?php echo $_GET[a]. ' '. $_GET[b] ?>
+    <?php if ($_GET['a'] % 4 == 0) echo ' Год високосный'; else echo ' Год не високосный';?>
 </body>
 </html>
